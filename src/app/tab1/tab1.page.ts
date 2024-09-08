04964acc3633cfcf1b583e562fdcbbe2e38b71e6
SrSebald <we.sebald@duocuc.cl>
2024-09-08 17:11:48 -0300

    Comit de prueba
    En este commit se intenta modificar el tab 1 para que sea como una entrada sobresaliente al momento de entrar.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonAvatar } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { addIcons } from "ionicons";
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, ExploreContainerComponent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButton,
+    IonAvatar,  // <-- Add IonAvatar here
+    ExploreContainerComponent
+  ],
 })
 export class Tab1Page {
   constructor() {}
