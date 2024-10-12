1d6207f2bd5107b42649d90ba896d54259ab0b52
SrSebald <we.sebald@duocuc.cl>
2024-10-11 21:14:49 -0300

    Now we have page 404 for technical problems
    In this commit, we created page 404 for any special occasion

--- src/app/tab2/tab2.page.spec.ts
+++ src/app/tab2/tab2.page.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {NavController} from '@ionic/angular';
 import { Tab2Page } from './tab2.page';
+
 
 describe('Tab2Page', () => {
   let component: Tab2Page;
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonImg } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonImg, IonItem, IonLabel, IonButton } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
-
+import {Router} from '@angular/router'; 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonImg]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonImg, IonItem, IonLabel, IonButton]
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(private route: Router) { }
+
+  nextpage() {
+    this.route.navigate(['/home']);
+  }
 
 }
