ca264da6a67b196718edf9c93d0797d01fd322ad
SrSebald <we.sebald@duocuc.cl>
2024-10-22 21:30:46 -0300

    cambiando la página not found
    Se ha actualizado el componente de notfound

--- src/app/notfound/notfound.component.ts
+++ src/app/notfound/notfound.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonImg, IonItem, IonLabel, IonButton } from '@ionic/angular/standalone';
+import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-notfound',
   templateUrl: './notfound.component.html',
   styleUrls: ['./notfound.component.scss'],
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonImg, IonItem, IonLabel, IonButton]
 })
-export class NotfoundComponent  implements OnInit {
 
-  constructor() { }
+export class NotfoundComponent   {
 
-  ngOnInit() {}
+  constructor(private route: Router) { }
+
+  nextpage() {
+    this.route.navigate(['/home']);
+  }
 
 }
--- src/app/tab1/tab1.page.spec.ts
+++ src/app/tab1/tab1.page.spec.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Tab1Page } from './tab1.page';
 
