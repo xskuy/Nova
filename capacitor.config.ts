98936acf3d4fc643d690dec0a7dde6657921721a
xskuy <benj123a@gmail.com>
2024-11-10 21:23:13 -0300

    IOS Build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,29 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.tuapp.id',
   appName: 'Nova',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    androidScheme: 'https',
+    cleartext: true,
+    allowNavigation: ['*']
+  },
+  ios: {
+    contentInset: 'always',
+    scrollEnabled: true,
+    limitsNavigationsToAppBoundDomains: false,
+    preferences: {
+      allowsBackgroundWebSocket: true,
+      allowsInlineMediaPlayback: true
+    }
+  },
+  plugins: {
+    FirebaseAuthentication: {
+      skipNativeAuth: false,
+      providers: ['apple.com', 'google.com']
+    }
+  }
 };
 
 export default config;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC8dpPAnfe1lee1E8K1YzAHrvym7xXBnfI",
+    authDomain: "nova-7a17a.firebaseapp.com",
+    projectId: "nova-7a17a",
+    storageBucket: "nova-7a17a.appspot.com",
+    messagingSenderId: "1021706714950",
+    appId: "1:1021706714950:web:d5b9406b48e95fefa5af47",
+    measurementId: "G-FB093SB7NS"
+  }
 };
